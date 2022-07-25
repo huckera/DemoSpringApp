db527ff523db764b6cc0b941ed8cbbae6207168e
veresardor <ardoragent@gmail.com>
2022-07-25 15:00:56 +0200

    introduce add+update collab functions to frontend

--- src/main/java/org/ao/collman/servicecontroller/CollaboratorController.java
+++ src/main/java/org/ao/collman/servicecontroller/CollaboratorController.java
@@ -12,12 +12,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,25 +30,25 @@ public class CollaboratorController {
 
 	private static final Logger log = LoggerFactory.getLogger(CollaboratorController.class);
 
-	@RequestMapping(path = "/add", method = RequestMethod.POST, produces = {"application/json"})
-	public ResponseEntity<List<Collaborator>> addCollaborator(@RequestBody Collaborator collaborator) {
+	@RequestMapping(path = "/add", method = RequestMethod.POST)
+	public ResponseEntity<String> addCollaborator(@Valid Collaborator collaborator) {
 		log.info("Request received addcollaborator: " + collaborator);
-		try {
-			log.debug("Calling service addcollaborator: " + collaborator);
-			services.addCollaborator(collaborator);
-
-			log.info("Start displaying collaborator list");
-			log.debug("Calling service listcollaborator");
-			List<Collaborator> collaboratorList = (List<Collaborator>) services.listAllCollaborators();
-			return new ResponseEntity<List<Collaborator>>(collaboratorList, HttpStatus.OK);
-		} catch (Exception e) {
-			// TODO addCollaborator error page
-			List<Collaborator> collaboratorList = (List<Collaborator>) services.listAllCollaborators();
-			return new ResponseEntity<List<Collaborator>>(collaboratorList, HttpStatus.INTERNAL_SERVER_ERROR);
-		}
+		log.debug("Calling service addcollaborator: " + collaborator);
+		services.addCollaborator(collaborator);
+		return new ResponseEntity<String>("Collaborator successfully added", HttpStatus.OK);
 	}
 
-	@GetMapping(path = "/list", produces = {"application/json"})
+	@GetMapping(path = "get/{id}", produces = {"application/json"})
+	public ResponseEntity<Collaborator> getCollaborator(@PathVariable("id") Integer id) {
+
+		log.info("Request received getCollaborator");
+		log.debug("Calling service getCollaborator");
+		Collaborator result = (Collaborator) services.getCollaboratorById(id);
+		log.debug("Returning result:" + result);
+		return new ResponseEntity<Collaborator>(result, HttpStatus.OK);
+	}
+	
+	@GetMapping(path = "list", produces = {"application/json"})
 	public ResponseEntity<List<Collaborator>> listAllCollaborators(Model model) {
 
 		log.info("Request received listAllCollaborator");
@@ -60,27 +58,22 @@ public class CollaboratorController {
 	}
 
 	@PostMapping("update/{id}")
-	public String updateCollaborator(@PathVariable("id") Integer id, @Valid Collaborator updatedCollaborator, BindingResult result, Model model) {
+	public ResponseEntity<String> updateCollaborator(@PathVariable("id") Integer id, @Valid Collaborator updatedCollaborator) {
 		log.info("Request received updateCollaborator with id: " + id + ", collaborator changes: " + updatedCollaborator);
-		if (result.hasErrors()) {
-			updatedCollaborator.setId(id);
-			return "update-collab";
-		}
-		log.debug("Calling service addcollaborator: " + updatedCollaborator);
+		log.debug("Calling service updateCollaborator: " + updatedCollaborator);
 		services.addCollaborator(updatedCollaborator);
-		return "updatesuccess";
+		return new ResponseEntity<String>("Collaborator successfully updated", HttpStatus.OK);
 	}
 
 	@DeleteMapping("delete/{id}")
-	public String deleteCollaborators(@PathVariable("id") Integer collaboratorId, Model model) {
+	public ResponseEntity<String> deleteCollaborators(@PathVariable("id") Integer collaboratorId) {
 
 		log.info("Request received deleteCollaborator with id: " + collaboratorId);
 		log.debug("Calling service deleteCollaborator");
 		services.deleteCollaborator(collaboratorId);
+		
+		return new ResponseEntity<String>("Collaborator successfully deleted", HttpStatus.OK);
 
-		Iterable<Collaborator> collaboratorList = services.listAllCollaborators();
-		model.addAttribute("Collaborators", collaboratorList);
-		return "list-collabs";
 	}
 
 }
--- src/main/resources/static/list-collabs.js
+++ src/main/resources/static/list-collabs.js
@@ -31,6 +31,65 @@ utils.get = (url) => {
   });
 }
 
+utils.post = (url,data) => {
+
+	var req = new XMLHttpRequest();
+	
+	req.open('POST',url);
+	req.setRequestHeader('Content-type','application/json; charset=utf-8');
+	
+	req.onload = function() {
+		// handle both remote 200 responses and local zero responses...
+	    if (req.status == 200)  {
+	        console.log('Update was successful')
+	    }
+	    else {
+	      reject(Error('promise error with ' + req.status));
+	    }
+	};
+	
+	req.onerror = function(err) {
+	    reject(Error('Network Error with '+url+': ' + err));
+	};
+
+  	// optional
+  	req.onreadystatechange = function(m) {
+     	console.log('m: ' + m);
+  	};
+
+	var json = JSON.stringify(data);
+		req.send(json);
+}
+
+utils.delete = (url) => {
+
+  var req = new XMLHttpRequest();
+
+  req.open('DELETE',url);
+
+  req.onload = function() {
+    // handle both remote 200 responses and local zero responses...
+    if (req.status == 200)  {
+        console.log('Delete was successful')
+    }
+    else {
+      reject(Error('promise error with ' + req.status));
+    }
+  };
+
+  req.onerror = function(err) {
+    reject(Error('Network Error with '+url+': ' + err));
+  };
+
+  // optional
+  req.onreadystatechange = function(m) {
+     console.log('m: ' + m);
+  };
+
+  req.send();
+}
+
+
 utils.getJSON = async function(url) {
   
   	var data = {};
@@ -71,21 +130,54 @@ async function init() {
 
 function prepareHtml(dataList) {
   
-  let html = ''
+	let html = '<table border="1"><tr><th>Name</th><th>Email</th><th>Edit link</th><th>Delete link</th></tr>'
   
-  for (data of dataList) {
+	for (data of dataList) {
    
-    // start a section element for each element
-    html += '<section>';
+    	// start a table row for each element
+    	html += '<tr>';
+  
+	    // create a column for each field + edit link + delete link
+    	html += `<td>${data.name}</td>`
+		html += `<td>${data.email}</td>`
+		html += `<td><a href="javascript:initUpdate('${data.id}')">Edit</a></td>`
+		html += `<td><a href="javascript:utils.delete('/collab/delete/${data.id}')">Delete</a></td>`
+		
+    	// end a table row for each element
+    	html += '</tr>';
     
-    // create a <div> for each key-value pair
-    for (key in data) {
-      
-      html += `<div><strong>${key}</strong>: ${data[key]}</div>`
-    }
-    // close off the section
-    html += '</section>';
+	}
+    
+    // close off the table
+    html += '</table>';
+  
+  	return html;
+}
+
+async function initUpdate(id) {
+  
+  let updateDiv = document.querySelector('#update');
+  let url = `/collab/get/${id}`;
+  //let url = 'https://jsonplaceholder.typicode.com/albums';
+  let collab = null;
+  try {
+     collab = await utils.getJSON(url);
   }
-  // return the html
-  return html;
+  catch (e) {
+     root.textContent = 'error: ' + e;
+  }
+  updateDiv.innerHTML = prepareUpdateHtml(collab);
+}
+
+function prepareUpdateHtml(data) {
+  
+	let html = `<form action="/collab/update/${data.id}" method="post" target="_top">`
+	html += `<label for="name" class="col-form-label">Name</label>`
+	html += `<input type="text" id="name" name="name" value="${data.name}"><br>`
+	html += `<label for="email" class="col-form-label">Email</label>`
+	html += `<input type="text" id="email" name="email" value="${data.email}"><br>`
+	html += `<input type="submit" value="Update Collaborator">`
+    html += `</form>`;
+  
+  	return html;
 }
